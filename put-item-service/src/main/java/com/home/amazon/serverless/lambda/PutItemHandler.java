559c482d2cd8792f8edfa040ec7d2a8d6113f2d1
Rostyslav_Myronenko <rimironenko@gmail.com>
2021-07-20 13:52:42 +0300

    Added PutItem service

--- put-item-service/src/main/java/com/home/amazon/serverless/lambda/PutItemHandler.java
+++ put-item-service/src/main/java/com/home/amazon/serverless/lambda/PutItemHandler.java
@@ -10,41 +10,37 @@ import com.home.amazon.serverless.model.Book;
 import org.apache.commons.lang3.StringUtils;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 import java.util.Collections;
-import java.util.Map;
 
-public class GetItemHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class PutItemHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     private final DynamoDbEnhancedClient dbClient;
     private final String tableName;
     private final TableSchema<Book> bookTableSchema;
 
-    public GetItemHandler() {
+    public PutItemHandler() {
         dbClient = DependencyFactory.dynamoDbEnhancedClient();
         tableName = DependencyFactory.tableName();
         bookTableSchema = TableSchema.fromBean(Book.class);
     }
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        String response = StringUtils.EMPTY;
-        DynamoDbTable<Book> booksTable = dbClient.table(tableName, bookTableSchema);
-        Map<String, String> pathParameters = input.getPathParameters();
-        if (pathParameters != null) {
-            String itemPartitionKey = pathParameters.get(Book.PARTITION_KEY);
-            Book item = booksTable.getItem(Key.builder().partitionValue(itemPartitionKey).build());
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        String body = request.getBody();
+        int statusCode = 204;
+        if (StringUtils.isNotBlank(body)) {
+            Book item = new Gson().fromJson(body, Book.class);
             if (item != null) {
-                response = new Gson().toJson(item);
+                DynamoDbTable<Book> booksTable = dbClient.table(tableName, bookTableSchema);
+                booksTable.putItem(item);
+                statusCode = 201;
             }
         }
-
-        return new APIGatewayProxyResponseEvent().withStatusCode(200)
+        return new APIGatewayProxyResponseEvent().withStatusCode(statusCode)
                 .withIsBase64Encoded(Boolean.FALSE)
-                .withHeaders(Collections.emptyMap())
-                .withBody(response);
+                .withHeaders(Collections.emptyMap());
     }
 
 }
